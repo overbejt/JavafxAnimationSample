4c85b3eabc6073b14c654118a6579af0166a4bc5
Josh Overbeck <overbejt@miamioh.edu>
2017-11-24 19:15:10 -0500

    First commit

---
+++ src/animationsample/AnimationSample.java
@@ -0,0 +1,36 @@
+
+package animationsample;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+
+public class AnimationSample extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) {
+        
+        
+        Pane root = new Pane();
+        
+        Scene scene = new Scene(root, 600, 600);
+        
+        primaryStage.setTitle("Hello World!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}

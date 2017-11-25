852cf4c097a445435c1800f499200216146da91e
Josh Overbeck <overbejt@miamioh.edu>
2017-11-24 19:42:28 -0500

    Added a circle

--- src/animationsample/AnimationSample.java
+++ src/animationsample/AnimationSample.java
@@ -8,6 +8,8 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
 
@@ -16,12 +18,17 @@ public class AnimationSample extends Application {
     @Override
     public void start(Stage primaryStage) {
         
+        Circle cir = new Circle();
+        cir.setFill(Color.CADETBLUE);
+        cir.setRadius(30);
+        cir.relocate(15,15);
         
         Pane root = new Pane();
+        root.getChildren().add(cir);
         
         Scene scene = new Scene(root, 600, 600);
         
-        primaryStage.setTitle("Hello World!");
+        primaryStage.setTitle("Tansition!");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
